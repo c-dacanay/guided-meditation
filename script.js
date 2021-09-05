29ba3306dcf00bbc8a47883e00d9f41f7a32dae5
c-dacanay <tina.dacanay@gmail.com>
2021-09-05 15:47:16 -0400

    kinda getting there

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
-console.log('ayooooo')
 let colorFeel = document.getElementById('colorfeel');
 let defaultColor = "#0000ff";
 let praySequence = [];
 let sequence = 0;
-
-let feeling = document.getElementById('feel')
 let keySequence = document.getElementsByClassName('svgKey')
-console.log(keySequence)
+let prayerInput = document.getElementById('prayerInput')
+let praying = true;
+let debugPray = ['feeling', 'mantra', 'blessing', 'beautiful']
+
+if (praying) {
+    let hide = document.getElementById('intros');
+    hide.style.display = 'none';
+    console.log('true');
+    document.getElementById('prayer').style.display = 'block';
+    praySequence = debugPray
+    pray();
+}
+
+function pray(){
+    let prayerDiv = document.getElementById('prayerDisplay');
+    prayerDiv.innerHTML = praySequence[0]
+    prayerInput.addEventListener("keypress", submitOnEnter)
+}
+
+function submitOnEnter(event) {
+    if(event.which === 13){
+        event.preventDefault(); // Prevents the addition of a new line in the text field (not needed in a lot of cases)
+        checkStrings();
+    }
+}
+
+function checkStrings(){
+    console.log(prayerInput.value, praySequence[sequence])
+    if (prayerInput.value === praySequence[sequence]){
+        console.log('yeee')
+    }
+}
 
 // Get all the inputs
-function prayer(a) {
-    console.log(a)
+function prayerPrep(a) {
+    // console.log(a)
     //put the responses into an array
     if (a) {
         let question = document.getElementById(a).value
         praySequence.push(question)
-        console.log(praySequence)
     }
     keySequence[sequence].style.fill = 'white';
     sequence++
-    console.log(keySequence[sequence])
+    if (sequence >= 4) {
+        praying = true;
+        console.log('help')
+    }
+    console.log(praySequence)
 }
 
 //Colors
